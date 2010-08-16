a5917a4d20bf3d9c133cb097f598da741008cc66
cdtsang <cdtsang@b0d08c91-5503-0410-b283-c95c722f64d4>
2010-08-16 17:40:38 +0000

    getInnerHtml so that one can see the mark up
    git-svn-id: https://subversion.calgbapps.org/repos/dev/PerformanceToolBox/trunk@30970 b0d08c91-5503-0410-b283-c95c722f64d4

--- src/main/java/org/calgb/test/performance/html/HtmlContent.java
+++ src/main/java/org/calgb/test/performance/html/HtmlContent.java
@@ -3,48 +3,58 @@ package org.calgb.test.performance.html;
 import java.util.HashMap;
 import java.util.Vector;
 
-
-
 public class HtmlContent {
-	private HtmlElement element;
-	private String innerHtml;
-	
-	HtmlContent(String html){
-		
-	}
-	HtmlContent(HtmlElement htmlElement, String innerHtml){
-		this.element =htmlElement;
-		this.innerHtml = innerHtml;
-	}
-	
-	public HtmlElement getElement(){
-		return element;
-	}
-	
-	HashMap<String, String> getAttributes(RegexMatch match, String tagName) {
-		HashMap<String, String> attrPairs = new HashMap<String, String>();
-		String tagDeclaration = match.find("<" + tagName + ".*?>").get(0)
-				.getText();
-		tagDeclaration = tagDeclaration.replaceAll("(<form)|(\\/*>$)|\\\"", "");
-		String[] tagPairs = tagDeclaration.split("\\s");
-		for (String tagPair : tagPairs) {
-			if (tagPair.contains("=")) {
-				int eqlIdx = tagPair.indexOf("=");
-				attrPairs.put(tagPair.substring(0, eqlIdx), tagPair
-						.substring(eqlIdx + 1));
-			}
-		}
-		return attrPairs;
-	}
-	
-	public HtmlElement find(String tagName, String attribute, String value){
-			Vector<RegexMatch> matches = new RegexMatch(innerHtml
-					.replaceAll("\\n|\\r", "")).find("<"+tagName+"[^>]+"+attribute +"="+ "\""+ value+"\""+".*?\\/>");
-			
-			for(RegexMatch match :matches){
-				HashMap<String, String> attributes = getAttributes(match, tagName);
-				return new HtmlElement(attributes);
-			}
-			return null;
-	}
+    private HtmlElement element;
+    private String      innerHtml;
+
+    HtmlContent(final String html)
+        {
+
+        }
+
+    HtmlContent(final HtmlElement htmlElement, final String innerHtml)
+        {
+            this.element = htmlElement;
+            this.innerHtml = innerHtml;
+        }
+
+    public HtmlElement getElement()
+        {
+            return element;
+        }
+
+    HashMap<String, String> getAttributes(final RegexMatch match, final String tagName)
+        {
+            final HashMap<String, String> attrPairs = new HashMap<String, String>();
+            String tagDeclaration = match.find("<" + tagName + ".*?>").get(0).getText();
+            tagDeclaration = tagDeclaration.replaceAll("(<form)|(\\/*>$)|\\\"", "");
+            final String[] tagPairs = tagDeclaration.split("\\s");
+            for (final String tagPair : tagPairs)
+                {
+                    if (tagPair.contains("="))
+                        {
+                            final int eqlIdx = tagPair.indexOf("=");
+                            attrPairs.put(tagPair.substring(0, eqlIdx), tagPair.substring(eqlIdx + 1));
+                        }
+                }
+            return attrPairs;
+        }
+
+    public String getInnerHtml()
+        {
+            return innerHtml;
+        }
+
+    public HtmlElement find(final String tagName, final String attribute, final String value)
+        {
+            final Vector<RegexMatch> matches = new RegexMatch(innerHtml.replaceAll("\\n|\\r", "")).find("<" + tagName + "[^>]+" + attribute
+                    + "=" + "\"" + value + "\"" + ".*?\\/>");
+
+            for (final RegexMatch match : matches)
+                {
+                    final HashMap<String, String> attributes = getAttributes(match, tagName);
+                    return new HtmlElement(attributes);
+                }
+            return null;
+        }
 }
